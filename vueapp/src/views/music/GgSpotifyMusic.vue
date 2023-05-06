25f57ee674c900d42be9c0844862eb60101471a7
Dimitri Steinbusch <dimitri.steinbusch@creatiview.be>
2023-05-06 15:07:19 +0200

    Add new album to spotify page

--- vueapp/src/views/music/GgSpotifyMusic.vue
+++ vueapp/src/views/music/GgSpotifyMusic.vue
@@ -1,18 +1,34 @@
 <template>
-    <iframe
-        style="border-radius:12px"
-        src="https://open.spotify.com/embed/album/3LPY9YtJT3rLDZRM53TYJQ?utm_source=generator&theme=0"
-        width="100%"
-        height="380"
-        frameBorder="0"
-        allowfullscreen="true"
-        allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
-    />
+    <div class="spotifyAlbums">
+        <iframe style="border-radius:12px"
+                src="https://open.spotify.com/embed/album/6sGDwcqW6KWNSkyfdLFK4V?utm_source=generator&theme=0"
+                width="100%"
+                height="380"
+                frameBorder="0"
+                allowfullscreen="true"
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        />
+
+        <iframe
+                style="border-radius:12px"
+                src="https://open.spotify.com/embed/album/3LPY9YtJT3rLDZRM53TYJQ?utm_source=generator&theme=0"
+                width="100%"
+                height="380"
+                frameBorder="0"
+                allowfullscreen="true"
+                allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture"
+        />
+    </div>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style scoped>
+.spotifyAlbums {
+    display: flex;
+    flex-direction: column;
+    row-gap: 50px;
+}
 
 </style>
